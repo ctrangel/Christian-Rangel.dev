fb4ecd61782e8b398f98b1e6a853182bdd5662c4
ctrangel <ctrangel@pointpark.edu>
2023-03-04 01:35:49 -0500

    Add: comments and format changes
    For readability

--- js/script.js
+++ js/script.js
@@ -12,7 +12,23 @@ $(document).ready(function() {
         'scrollTop': $target.offset().top
       }, 900, 'swing', function () {
         window.location.hash = target;
+
       });
+      let name = document.getElementById("name-nav");
+      name.classList.add("name-pulse");
+      console.log("function called");
     });
   });
   
+  function addPulseOnScroll(id, className) {
+    window.addEventListener('scroll', function() {
+      let ele = document.getElementById(id);
+      if (window.scrollY > 0) {
+        ele.classList.add(className);
+      } else {
+        ele.classList.remove(className);
+      }
+    });
+  }
+  
+  addPulseOnScroll("name-nav", "name-pulse");
